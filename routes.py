2e7cf9c978285955ee05f1a60ede12178eb249b2
nishijayaraj <nishijayaraj@gmail.com>
2018-11-01 20:23:25 +0530

    Login: session and POST

--- routes.py
+++ routes.py
@@ -1,6 +1,17 @@
 from mysite import app
-from flask import render_template
+from flask import render_template,session,request
+
 @app.route('/')
 def home():
-    return render_template('login.html')
-    
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
+ 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
