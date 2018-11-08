9f7ac74ca5d0864cb69e75ccd87056756deca1e5
nishijayaraj <nishijayaraj@gmail.com>
2018-11-08 20:01:22 +0530

    Data passing to template: Added home page

--- routes.py
+++ routes.py
@@ -2,19 +2,23 @@ from mysite import app
 from flask import render_template,session,request,flash
 
 @app.route('/')
-def home():
+def home(actor=None):
+    actor =actor or ''
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return "Hello Boss!"
+        return render_template('home.html',actor=actor)
  
 @app.route('/login', methods=['POST'])
 def do_admin_login():
+    user='' 
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
         session['logged_in'] = True
+        user = 'admin'
     else:
         flash('wrong password!')
-    return home()
+        user = None 
+    return home(user)
 
 @app.route("/logout")
 def logout():
